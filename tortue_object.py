52171030ad09a17ca44665869d6a586910ff38ba
39Olivier <boite151@hotmail.fr>
2019-10-24 10:35:26 +0200

    1er commit

--- tortue_object.py
+++ tortue_object.py
@@ -1,20 +1,26 @@
 import turtle
 from random import randint
 
+# region configuration fenetre
+fond = (randint(0, 255), randint(0, 255), randint(0, 255))
 wn = turtle.Screen()
-Pen_1 = turtle.Turtle()
 wn.colormode(255)
+wn.bgcolor(fond)
+# endregion
 
-color = (randint(0, 255), randint(0, 255), randint(0, 255))
-
-Pen_1.shape('arrow')
-Pen_1.pencolor(color)
+pen_1 = turtle.Turtle()
 
 
-Pen_1.forward(150)
-Pen_1.left(75)
-Pen_1.forward(75)
-Pen_1.left(75)
-Pen_1.forward(75)
+pen_1.shape('arrow')
+length = 100
 
+for i in range(20):
+    color = (randint(0, 255), randint(0, 255), randint(0, 255))
+    pen_1.pencolor(color)
+    pen_1.forward(150)
+    pen_1.left(75)
+    pen_1.forward(length)
+    pen_1.left(75)
+    pen_1.forward(75)
+    length = length + 30
 wn.exitonclick()
