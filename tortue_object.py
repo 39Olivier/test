39b6fde14e7804f2536708dab933f550f9256652
39Olivier <boite151@hotmail.fr>
2019-10-25 11:36:31 +0200

    1er commit

--- tortue_object.py
+++ tortue_object.py
@@ -1,26 +1,33 @@
 import turtle
 from random import randint
 
-# region configuration fenetre
-fond = (randint(0, 255), randint(0, 255), randint(0, 255))
+
+def tuple_random_rgb():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    return r, g, b
+
+
 wn = turtle.Screen()
 wn.colormode(255)
-wn.bgcolor(fond)
-# endregion
+wn.bgcolor(tuple_random_rgb())
 
 pen_1 = turtle.Turtle()
-
-
 pen_1.shape('arrow')
 length = 100
 
-for i in range(20):
-    color = (randint(0, 255), randint(0, 255), randint(0, 255))
+for i in range(10):
+    color = tuple_random_rgb()
     pen_1.pencolor(color)
+    pen_1.pensize(10)
+    pen_1.speed(5)
+    # pen_1.circle(50, 150)
     pen_1.forward(150)
     pen_1.left(75)
     pen_1.forward(length)
     pen_1.left(75)
     pen_1.forward(75)
     length = length + 30
+
 wn.exitonclick()
